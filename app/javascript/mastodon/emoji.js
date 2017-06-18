e1334801acb1e07c538d42ed865772e451695682
MIYAGI Hikaru <hikaru.devel@js4.in>
2017-06-18 19:39:19 +0900

    絵文字機能強化

--- app/javascript/mastodon/emoji.js
+++ app/javascript/mastodon/emoji.js
@@ -30,8 +30,78 @@ const shortnameToImage = str => str.replace(emojione.regShortNames, shortname =>
   return `<img draggable="false" class="emojione" alt="${alt}" title="${shortname}" src="/emoji/${unicode}.svg" />`;
 });
 
-export default function emojify(text) {
-  return toImage(text).replace(/5,?000\s*兆円/g, (m) => {
-    return `<img alt="${m}" src="/emoji/5000tyoen.svg"/>`
+const hesc = raw => {
+  var ent = false;
+  return raw.replace(/./ug, c => {
+    if (ent) {
+      if (c == ';') ent = false;
+    }
+    else if (c == '&') {
+       ent = true;
+    }
+    else {
+      c = `&#${c.codePointAt(0)};`;
+    }
+    return c;
   })
+};
+
+const localEmoji = {
+  pre: [
+    {
+      re: /((?:✨|(?::sparkles:))+)( ?IPv6[^<>\s]*? ?)((?:✨|(?::sparkles:))+)/ug,
+      fmt: (m, s1, ip, s2) => {
+        var f = k => k.replace(/✨/g, "<kira1/>").replace(/:sparkles:/g, "<kira2/>");
+        
+        for (var iptrim = ip.slice(ip.indexOf("IPv6") + 4), len = 0; iptrim.length && len < 7; len++) {
+          if (iptrim[0] == ":") {
+            var rr = /^:[\w\d-]+:/.exec(iptrim);
+            if (!rr) return m;
+            iptrim = iptrim.slice(rr[0].length);
+          }
+          else if (iptrim[0] == "&") {
+            iptrim = iptrim.replace(/&.*?;/, "");
+          }
+          else if (iptrim.codePointAt(0) >= 65536) {
+            iptrim = iptrim.slice(2);
+          }
+          else {
+            iptrim = iptrim.slice(1);
+          }
+        }
+        if (iptrim.length) return m;
+        return `${f(s1)}<ipv6>${ip}</ipv6>${f(s2)}`;
+      }
+    },
+  ],
+  stamp: [
+    {re: /5,?000\s?兆円/g, img: '5000tyoen.svg'},
+    {re: /5,?000兆/g, img: '5000tyo.svg'},
+  ],
+  post_raw: [],
+  post_all: [
+    {re: /<kira1\/>/g, fmt: () => `<span class="v6don-kira">${unicodeToImage("✨")}</span>`},
+    {re: /<kira2\/>/g, fmt: () => `<span class="v6don-kira">${unicodeToImage(":sparkles:")}</span>`},
+    {re: /<ipv6>(.*?)<\/ipv6>/mg, fmt: (m, ip) => `<span class="v6don-nobi">${ip}</span>`},
+  ],
+};
+
+export default function emojify(text) {
+  var tr = arr =>
+    arr.reduce((t, e) =>
+      t.replace(/([^<]*)(<[^>]*>)?/mg, (all, raw, tag) =>
+        raw.replace(e.re, e.fmt) + (tag || '')), text)
+
+  text = tr(localEmoji.pre)
+  
+  text = toImage(text);
+  
+  text = tr(localEmoji.stamp.map(e => {
+    e.fmt = (m) => `<img alt="${hesc(m)}" src="/emoji/${e.img}"/>`;
+    return e;
+  }))
+  text = tr(localEmoji.post_raw);
+  text = localEmoji.post_all.reduce((t, e) => t.replace(e.re, e.fmt), text);
+  
+  return text;
 };
