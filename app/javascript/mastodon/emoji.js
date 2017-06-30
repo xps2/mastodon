b9a2b3f2537db135d0689c8f42c969af8c0f03f5
MIYAGI Hikaru <hikaru.devel@js4.in>
2017-07-01 02:54:10 +0900

    simplify shortnameToImage()

--- app/javascript/mastodon/emoji.js
+++ app/javascript/mastodon/emoji.js
@@ -20,41 +20,25 @@ const unicodeToImage = str => {
   });
 };
 
-const shortnameToImage = str => {
-  // This walks through the string from end to start, ignoring any tags (<p>, <br>, etc.)
-  // and replacing valid shortnames like :smile: and :wink: that _aren't_ within
-  // tags with an <img> version.
-  // The goal is to be the same as an emojione.regShortNames replacement, but faster.
-  // The reason we go backwards is because then we can replace substrings as we go.
-  let i = str.length;
-  let insideTag = false;
+const shortnameToImage = str => str.replace(/([^<]*)(<[^>]*>)?/mg, (all, raw, tag) => {
   let insideShortname = false;
-  let shortnameEndIndex = -1;
-  while (i--) {
-    const char = str.charAt(i);
-    if (insideShortname && char === ':') {
-      const shortname = str.substring(i, shortnameEndIndex + 1);
+  return raw.split(":").reduce((rtn, shortname) => {
+    if (insideShortname) {
       if (shortname in emojione.emojioneList) {
         const unicode = emojione.emojioneList[shortname].unicode[emojione.emojioneList[shortname].unicode.length - 1];
         const alt = emojione.convert(unicode.toUpperCase());
-        const replacement = `<img draggable="false" class="emojione" alt="${alt}" title="${shortname}" src="/emoji/${unicode}.svg" />`;
-        str = str.substring(0, i) + replacement + str.substring(shortnameEndIndex + 1);
-      } else {
-        i++; // stray colon, try again
+        rtn += `<img draggable="false" class="emojione" alt="${alt}" title="${shortname}" src="/emoji/${unicode}.svg" />`;
+        insideShortname = false;
       }
-      insideShortname = false;
-    } else if (insideTag && char === '<') {
-      insideTag = false;
-    } else if (char === '>') {
-      insideTag = true;
-      insideShortname = false;
-    } else if (!insideTag && char === ':') {
-      insideShortname = true;
-      shortnameEndIndex = i;
+      else rtn += ":" + shortname;
     }
-  }
-  return str;
-};
+    else {
+      rtn += shortname;
+      insideShortname = true;
+    }
+    return rtn;
+  }, "") + (tag || "");
+});
 
 export default function emojify(text) {
   return toV6donEmoji("post", toImage(toV6donEmoji("pre", text)));
