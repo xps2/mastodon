e10566780861b0d2bfb2803f75b30864feb7f47a
MIYAGI Hikaru <hikaru.devel@js4.in>
2017-07-26 23:34:52 +0900

    絵文字処理いじり
    - emojify()から再帰処理を無くしてみたけどこれで結局あまり本家と変わらなくなっててアレ
    - localEmojiのデータの持ち方を変える。これからは文字列一つを引数に取る関数の単純な配列になる

--- app/javascript/mastodon/emoji-v6don.js
+++ app/javascript/mastodon/emoji-v6don.js
@@ -1,3 +1,13 @@
+import Trie from 'substring-trie';
+import emojify from './emoji';
+
+const localEmoji = {
+  pre: [], post: []
+};
+export default function(order, text) {
+  return localEmoji[order].reduce((t, e) => e(t), text);
+};
+
 const hesc = raw => {
   let ent = false;
   return raw.replace(/./ug, c => {
@@ -14,21 +24,17 @@ const hesc = raw => {
   })
 };
 
-const localEmoji = {
-  pre: [
-    {re: /:[\dA-Fa-f]{2,4}:/g, fmt: af => hesc(af)},
-  ],
+const byre = {
+  pre: [],
   post: [
     {tag: true, re: /(<a\s[^>]*>)(.*?:don:.*?)<\/a>/mg, fmt: (all, tag, text) => tag + 
       text.replace(/:don:/g, hesc(":don:")) + "</a>"
     },
     {re: /:don:/g, fmt: `<a href="https://mstdn.maud.io/">:don:</a>`},
-    {tag: true, re: /<kira>(.*?)<\/kira>/mg, fmt: (m, ip) => `<span class="v6don-kira">${ip}</span>`},
-    {tag: true, re: /<ipv6 ([^>]*)>(.*?)<\/ipv6>/mg, fmt: (m, style, txt) => `<span class="v6don-wave" ${style}>${txt}</span>`},
   ],
 };
 
-localEmoji.post.push(...[
+byre.post.push(...[
   {re: /5,?000\s?兆円/g, img: '5000tyoen.svg'},
   {re: /5,?000兆/g, img: '5000tyo.svg'},
 ].map(e => {
@@ -36,20 +42,21 @@ localEmoji.post.push(...[
     return e;
 }));
 
-localEmoji.pre.push({
+byre.pre.push({
   re: /(‡+|†+)([^†‡]{1,30}?)(‡+|†+)/g,
   fmt: (m, d1, txt, d2) => {
     if (d1[0] != d2[0]) return m;
     return `<span class="v6don-tyu2"><span class="v6don-dagger">${d1}</span>${txt}<span class="v6don-dagger">${d2}</span></span>`;
-  }
+  },
 });
 
-localEmoji.pre.push({
-  re: /((?:✨|(?::sparkles:))+)( ?IPv6.{0,30}?)((?:✨|(?::sparkles:))+)/ug,
+byre.pre.push({
+  re: /(✨+)( ?IPv6[^✨]*)(✨+)/ug,
   fmt: (m, s1, ip, s2) => {
-    let f = k => k.replace(/✨|:sparkles:/g, s => `<kira>${s}</kira>`);
+    let f = k => k.replace(/✨/g, s => `<span class="v6don-kira">${s}</span>`);
     
     let ipdeco = ""
+    ip = emojify(ip);
     for (let chars = 0, delay = 0; ip.length && chars < 11; chars++) {
       let deco = true, decolen;
       let rr = /^\s/u.exec(ip);
@@ -69,12 +76,9 @@ localEmoji.pre.push({
         decolen = rr[0].length;
       }
       else if (ip[0] == "<") {
-        deco = false;
+        deco = /^<img\s/i.test(ip);
         rr = /<[^>]*?>/.exec(ip);
         decolen = rr[0].length;
-      }
-      else if ((rr = /^5,?000\s?兆円?/.exec(ip))) {
-        decolen = rr[0].length
       }
       else if (ip.codePointAt(0) >= 65536) {
         decolen = 2;
@@ -84,7 +88,7 @@ localEmoji.pre.push({
       }
       
       if (deco) {
-        ipdeco += `<ipv6 style="animation-delay: ${delay}ms">${ip.slice(0, decolen)}</ipv6>`;
+        ipdeco += `<span class="v6don-wave" style="animation-delay: ${delay}ms">${ip.slice(0, decolen)}</span>`;
         delay += 100;
       }
       else {
@@ -101,29 +105,59 @@ localEmoji.pre.push({
   }
 });
 
+const apply_without_tag = (str, f) => str.replace(/([^<]*)(<[^>]*>)?/mg, (all, raw, tag) => f(raw) + (tag || ''));
 
-localEmoji.post.push(...[
-  {re: /:realtek:/g, img: "../proprietary/realtek.svg", width: 4.92},
-].map(e => {
-  let style = e.width ? `style="width: ${e.width}em"` : "";
-  e.fmt = (m) => `<img class="emojione" alt="${m}" title="${m}" src="/emoji/v6don/${e.img}" ${style}/>`;
-  return e;
-}));
+localEmoji.pre.push(...byre.pre.map(e => e.tag ?
+  str => str.replace(e.re, e.fmt) :
+  str => apply_without_tag(str, s => s.replace(e.re, e.fmt))));
+localEmoji.post.push(...byre.post.map(e => e.tag ?
+  str => str.replace(e.re, e.fmt) :
+  str => apply_without_tag(str, s => s.replace(e.re, e.fmt))));
 
-localEmoji.post.push(...[
-  "nicoru", "iine", "irane", "mayo",
-].map(e => {
-  let rtn = {};
-  rtn.re = new RegExp(`:${e}(\\d*):`, "g");
-  rtn.fmt = (m, deg) => `<img class="emojione" alt="${m}" title="${m}" src="/emoji/v6don/${e}.svg" style="transform: rotate(${deg || 0}deg)"/>`
-  return rtn;
-}));
+const shorttab = {};
+[
+    "nicoru", "iine", "irane", "mayo",
+].forEach(name => {
+  shorttab[name] = {
+    remtest: rem => /^\d+$/.test(rem),
+    append: (_, rem) => rem ? `style="transform: rotate(${rem}deg)"` : '',
+  };
+});
+shorttab.realtek = {path: () => "/emoji/proprietary/realtek.svg", append: () => `style="width: 4.92em"`};
 
-
-export default function(order, text) {
-  return localEmoji[order].reduce((t, e) => e.tag ?
-    t.replace(e.re, e.fmt) :
-    t.replace(/([^<]*)(<[^>]*>)?/mg, (all, raw, tag) =>
-      raw.replace(e.re, e.fmt) + (tag || ''))
-  , text);
+const le_curry = (trie, replacer) => function le_rec(cur, prev = '') {
+  let idx = cur.indexOf(':');
+  if (idx == -1) return prev + cur;
+  prev += cur.slice(0, idx);
+  cur = cur.slice(idx + 1);
+  idx = cur.indexOf(':');
+  if (idx == -1) return prev + ':' + cur;
+  let tag = cur.slice(0, idx);
+  cur = cur.slice(idx);
+  let match = trie.search(tag);
+  if (!match) return le_rec(cur, prev + ':' + tag);
+  let rem;
+  if (tag == match) {
+    rem = null;
+  }
+  else if (shorttab[match].remtest) {
+    rem = tag.slice(match.length);
+    if (!shorttab[match].remtest(rem)) return le_rec(cur, prev + ':' + tag);
+  }
+  else {
+    return le_rec(cur, prev + ':' + tag);
+  }
+  return le_rec(cur.slice(1), prev + replacer(match, rem));
 };
+
+localEmoji.post.push(str => apply_without_tag(str, raw =>
+  le_curry(new Trie(Object.keys(shorttab)), (match, rem) => {
+    let name = match + (rem || '');
+    let rtn = `<img class="emojione" alt=":${name}:" title=":${name}:" src="`
+    rtn += (shorttab[match].path ? shorttab[match].path(match, rem) : `/emoji/v6don/${match}.svg`) + '"';
+    if (shorttab[match].append) {
+      rtn += ' ' + (shorttab[match].append(match, rem) || '');
+    }
+    rtn += "/>";
+    return rtn;
+  })(raw)));
--- app/javascript/mastodon/emoji.js
+++ app/javascript/mastodon/emoji.js
@@ -1,49 +1,32 @@
 import { unicodeMapping } from './emojione_light';
-import emojione from 'emojione';
 import Trie from 'substring-trie';
 import toV6donEmoji from './emoji-v6don';
 
 const trie = new Trie(Object.keys(unicodeMapping));
 
-const emojify = str => str.replace(/([^<]*)(<[^>]*>)?/mg, (all, raw, tag) => {
-  const u2i = txt => {
-    let match;
-    let rtn = "";
-    while (txt.length && !(match = trie.search(txt))) {
-      rtn += txt[0];
-      txt = txt.slice(1);
+const emojify = str => {
+  let rtn = "";
+  for (;;) {
+    let match, c, i = 0;
+    while (i < str.length && (c = str[i]) != '<' && c != '&' && !(match = trie.search(str.slice(i)))) {
+      i += str.codePointAt(i) < 65536 ? 1 : 2;
     }
-    if (match) {
-      const unicodeStr = match;
-      if (unicodeStr in unicodeMapping) {
-        const [filename, shortCode] = unicodeMapping[unicodeStr];
-        const alt      = unicodeStr;
-        const replacement = `<img draggable="false" class="emojione" alt="${alt}" title=":${shortCode}:" src="/emoji/${filename}.svg" />`;
-        rtn += replacement + u2i(txt.slice(unicodeStr.length));
-      }
+    if (i == str.length)
+      break;
+    else if (c == '<' || c == '&') {
+      let tagend = i + str.slice(i).indexOf(">;"["<&".indexOf(c)]) + 1;
+      if (tagend == i)
+        break;
+      rtn += str.slice(0, tagend);
+      str = str.slice(tagend);
+      continue;
     }
-    return rtn;
+    const [filename, shortCode] = unicodeMapping[match];
+    rtn += str.slice(0, i) + `<img draggable="false" class="emojione" alt="${match}" title=":${shortCode}:" src="/emoji/${filename}.svg" />`;
+    str = str.slice(i + match.length);
   }
-  
-  let insideShortname = false;
-  return raw.split(":").reduce((rtn, txt, idx, arr) => {
-    if (insideShortname) {
-      let shortname = `:${txt}:`;
-      if (idx != arr.length - 1 && shortname in emojione.emojioneList) {
-        const unicode = emojione.emojioneList[shortname].unicode[emojione.emojioneList[shortname].unicode.length - 1];
-        const alt = emojione.convert(unicode.toUpperCase());
-        rtn += `<img draggable="false" class="emojione" alt="${alt}" title="${shortname}" src="/emoji/${unicode}.svg" />`;
-        insideShortname = false;
-      }
-      else rtn += ":" + u2i(txt);
-    }
-    else {
-      rtn += u2i(txt);
-      insideShortname = true;
-    }
-    return rtn;
-  }, "") + (tag || "");
-});
+  return rtn + str;
+}
 
 const emojify_v6don = text =>
   toV6donEmoji("post", emojify(toV6donEmoji("pre", text)));
