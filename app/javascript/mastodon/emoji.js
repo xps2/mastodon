065317c4091dbc1a8085095925cdd88969c7b066
MIYAGI Hikaru <hikaru.devel@js4.in>
2017-08-02 18:41:43 +0900

    emojify() リファクタ

--- app/javascript/mastodon/emoji.js
+++ app/javascript/mastodon/emoji.js
@@ -2,14 +2,27 @@ import { unicodeMapping } from './emojione_light';
 import Trie from 'substring-trie';
 import highlight from './v6don-highlighter';
 
+const excluded = ["®", "©", "™"];
+const notintab = [];
+excluded.forEach(c => {
+  if (unicodeMapping[c] !== undefined) {
+    delete unicodeMapping[c];
+  }
+  else {
+    notintab.push(`<${c}>`);
+  }
+});
+if (notintab.length) {
+  console.warn(`Excluded character(s) for emojify() [${notintab.join(', ')}] are not defined in unicodeMapping. Please contact the webmaster to solve it.`)
+}
+
 const trie = new Trie(Object.keys(unicodeMapping));
-const excluded = "®©™";
 
 const emojify = str => {
   let rtn = "";
   for (;;) {
     let match, c, i = 0, tag;
-    while (i < str.length && (excluded.indexOf(c = str[i]) != -1 || (tag = "<&".indexOf(c)) == -1 && !(match = trie.search(str.slice(i))))) {
+    while (i < str.length && ((tag = "<&".indexOf(c = str[i])) == -1 && !(match = trie.search(str.slice(i))))) {
       i += str.codePointAt(i) < 65536 ? 1 : 2;
     }
     if (i == str.length)
