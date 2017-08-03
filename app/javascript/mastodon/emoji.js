4706db3d848e366a75b4d08f51c9b8e30979b703
MIYAGI Hikaru <hikaru.devel@js4.in>
2017-08-03 15:29:48 +0900

    はい

--- app/javascript/mastodon/emoji.js
+++ app/javascript/mastodon/emoji.js
@@ -2,20 +2,6 @@ import { unicodeMapping } from './emojione_light';
 import Trie from 'substring-trie';
 import highlight from './v6don-highlighter';
 
-const excluded = ["®", "©", "™"];
-const notintab = [];
-excluded.forEach(c => {
-  if (unicodeMapping[c] !== undefined) {
-    delete unicodeMapping[c];
-  }
-  else {
-    notintab.push(`<${c}>`);
-  }
-});
-if (notintab.length) {
-  console.warn(`Excluded character(s) for emojify() [${notintab.join(', ')}] is not defined in unicodeMapping. Please contact the webmaster to fix it.`)
-}
-
 const trie = new Trie(Object.keys(unicodeMapping));
 
 const emojify = str => {
@@ -33,14 +19,15 @@ const emojify = str => {
         break;
       rtn += str.slice(0, tagend);
       str = str.slice(tagend);
-      continue;
     }
-    const [filename, shortCode] = unicodeMapping[match];
-    rtn += str.slice(0, i) + `<img draggable="false" class="emojione" alt="${match}" title=":${shortCode}:" src="/emoji/${filename}.svg" />`;
-    str = str.slice(i + match.length);
+    else {
+      const [filename, shortCode] = unicodeMapping[match];
+      rtn += str.slice(0, i) + `<img draggable="false" class="emojione" alt="${match}" title=":${shortCode}:" src="/emoji/${filename}.svg" />`;
+      str = str.slice(i + match.length);
+    }
   }
   return rtn + str;
-}
+};
 
 const emojify_v6don = text => emojify(highlight(text));
   
--- app/javascript/mastodon/emojione_light.js
+++ app/javascript/mastodon/emojione_light.js
@@ -4,8 +4,16 @@
 const emojione = require('emojione');
 
 const mappedUnicode = emojione.mapUnicodeToShort();
+const excluded = ["®", "©", "™"];
 
-module.exports.unicodeMapping = Object.keys(emojione.jsEscapeMap)
+const keys = Object.keys(emojione.jsEscapeMap);
+excluded.forEach(c => {
+  let idx = keys.findIndex(k => k === c);
+  if (idx !== -1) {
+    keys.splice(idx, 1);
+  }
+});
+module.exports.unicodeMapping = keys
   .map(unicodeStr => [unicodeStr, mappedUnicode[emojione.jsEscapeMap[unicodeStr]]])
   .map(([unicodeStr, shortCode]) => ({ [unicodeStr]: [emojione.emojioneList[shortCode].fname, shortCode.slice(1, shortCode.length - 1)] }))
   .reduce((x, y) => Object.assign(x, y), { });
