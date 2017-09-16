17cc82fc825b6a8d2c4627b92dd51f8a56222ad7
MIYAGI Hikaru <hikaru.devel@js4.in>
2017-09-16 23:50:57 +0900

    Revert "correct URL pattern used in text length counter in WebUI (#4968)"
    This reverts commit 629fae8b3be3a04d5190a9048f0aa5a15bd17828.

--- app/javascript/mastodon/features/compose/util/counter.js
+++ app/javascript/mastodon/features/compose/util/counter.js
@@ -1,9 +1,7 @@
-import { urlRegex } from './url_regex';
-
 const urlPlaceholder = 'xxxxxxxxxxxxxxxxxxxxxxx';
 
 export function countableText(inputText) {
   return inputText
-    .replace(urlRegex, urlPlaceholder)
+    .replace(/https?:\/\/\S+/g, urlPlaceholder)
     .replace(/(?:^|[^\/\w])@(([a-z0-9_]+)@[a-z0-9\.\-]+)/ig, '@$2');
 };
