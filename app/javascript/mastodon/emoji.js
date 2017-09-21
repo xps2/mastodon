2b7350ec15b1f881c9e8f78a78b8f8a953e04701
MIYAGI Hikaru <hikaru.devel@js4.in>
2017-09-21 17:34:26 +0900

    emojify()リファクタ
    str.sliceが繰り返されていたのが気になったので

--- app/javascript/mastodon/emoji.js
+++ app/javascript/mastodon/emoji.js
@@ -9,39 +9,42 @@ const emojify = (str, customEmojis = {}) => {
   let rtn = '';
   for (;;) {
     let match, i = 0, tag;
-    while (i < str.length && (tag = '<&'.indexOf(str[i])) === -1 && str[i] !== ':' && !(match = trie.search(str.slice(i)))) {
+    while (i < str.length && (tag = '<&:'.indexOf(str[i])) === -1 && !(match = trie.search(str.slice(i)))) {
       i += str.codePointAt(i) < 65536 ? 1 : 2;
     }
-    if (i === str.length)
+    let replaceEnd, replacement = '';
+    if (i === str.length) {
       break;
-    else if (tag >= 0) {
-      const tagend = str.indexOf('>;'[tag], i + 1) + 1;
-      if (!tagend)
-        break;
-      rtn += str.slice(0, tagend);
-      str = str.slice(tagend);
     } else if (str[i] === ':') {
-      try {
-        // if replacing :shortname: succeed, exit this block with "continue"
-        const closeColon = str.indexOf(':', i + 1) + 1;
-        if (!closeColon) throw null; // no pair of ':'
+      const testsn = () => {
+        // if replacing :shortname: succeed, set replacement and return true
+        replaceEnd = str.indexOf(':', i + 1) + 1;
+        if (!replaceEnd) return false; // no pair of ':'
         const lt = str.indexOf('<', i + 1);
-        if (!(lt === -1 || lt >= closeColon)) throw null; // tag appeared before closing ':'
-        const shortname = str.slice(i, closeColon);
+        if (!(lt === -1 || lt >= replaceEnd)) return false; // tag appeared before closing ':'
+        const shortname = str.slice(i, replaceEnd);
         if (shortname in customEmojis) {
-          rtn += str.slice(0, i) + `<img draggable="false" class="emojione" alt="${shortname}" title="${shortname}" src="${customEmojis[shortname]}" />`;
-          str = str.slice(closeColon);
-          continue;
+          replacement = `<img draggable="false" class="emojione" alt="${shortname}" title="${shortname}" src="${customEmojis[shortname]}" />`;
+          return true;
         }
-      } catch (e) {}
-      // replacing :shortname: failed
-      rtn += str.slice(0, i + 1);
-      str = str.slice(i + 1);
+        return false;
+      };
+      if (!testsn()) {
+        replaceEnd = ++i;
+      }
+    } else if (tag >= 0) {
+      // <, &
+      replaceEnd = str.indexOf('>;'[tag], i + 1) + 1;
+      if (!replaceEnd) break;
+      i = replaceEnd;
     } else {
+      // matched to unicode emoji
       const [codeSeq, shortCode] = unicodeMapping[match];
-      rtn += str.slice(0, i) + `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="emojione" viewBox="0 0 1 1"><g><title>:${shortCode}:</title><desc>${match}</desc><use xlink:href="${sprites}#emoji-${codeSeq}"/></g></svg>`;
-      str = str.slice(i + match.length);
+      replacement = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="emojione" viewBox="0 0 1 1"><g><title>:${shortCode}:</title><desc>${match}</desc><use xlink:href="${sprites}#emoji-${codeSeq}"/></g></svg>`;
+      replaceEnd = i + match.length;
     }
+    rtn += str.slice(0, i) + replacement;
+    str = str.slice(replaceEnd);
   }
   return rtn + str;
 };
