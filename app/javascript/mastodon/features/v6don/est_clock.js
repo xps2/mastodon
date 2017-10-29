550b8a565dc2d67d67b0db6ee4074ea5fc347789
MIYAGI Hikaru <hikaru.devel@js4.in>
2017-10-30 05:22:06 +0900

    EST時計更新

--- app/javascript/mastodon/features/v6don/est_clock.js
+++ app/javascript/mastodon/features/v6don/est_clock.js
@@ -1,51 +1,99 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 
-const pad = s => s.toString().length === 1 ? '0' + s : s;
-const localtime = d => <time dateTime={d.toISOString().replace(/:\d\d\.\d+/, '')}>
-  {d.getFullYear()}-{pad(d.getMonth() + 1)}-{pad(d.getDate())}
-  {' ' + pad(d.getHours())}<span className='v6don-estclock-tick'>:</span>{pad(d.getMinutes())}
-</time>;
-const estTZ = 'Europe/Berlin';
-const estFmt = {
-  year: new Intl.DateTimeFormat('en', { year: 'numeric', timeZone: estTZ }).format,
-  month: new Intl.DateTimeFormat('en', { month: '2-digit', timeZone: estTZ }).format,
-  date: new Intl.DateTimeFormat('en', { day: '2-digit', timeZone: estTZ }).format,
-  hour: new Intl.DateTimeFormat('en', { hour: '2-digit', hour12: false, timeZone: estTZ }).format,
-  minute: new Intl.DateTimeFormat('en', { minute: '2-digit', timeZone: estTZ }).format,
-};
-const est = d => <time dateTime={d.toISOString().replace(/:\d\d\.\d+/, '')}>
-  {estFmt.year(d)}-{estFmt.month(d)}-{estFmt.date(d)}
-  {' ' + pad(estFmt.hour(d))}<span className='v6don-estclock-tick'>:</span>{pad(estFmt.minute(d))}
-</time>;
+class Time extends React.PureComponent {
 
-export default class ESTClock extends React.Component {
+  static propTypes = {
+    timeZone: PropTypes.string,
+  };
+
+  constructor() {
+    super();
+    this.tick = this.tick.bind(this);
+    this.d = new Date();
+    this.nodes = {};
+  }
+
+  componentWillMount() {
+    const tz = this.props.timeZone;
+    this.tzFmt = {
+      year: new Intl.DateTimeFormat('en', { year: 'numeric', timeZone: tz }).format,
+      month: new Intl.DateTimeFormat('en', { month: 'numeric', timeZone: tz }).format,
+      date: new Intl.DateTimeFormat('en', { day: 'numeric', timeZone: tz }).format,
+      hour: new Intl.DateTimeFormat('en', { hour: 'numeric', hour12: false, timeZone: tz }).format,
+      minute: new Intl.DateTimeFormat('en', { minute: 'numeric', timeZone: tz }).format,
+      second: d => d.getSeconds().toString(),
+    };
+  }
 
   componentDidMount() {
-    this.prevTick = -Infinity;
-    this.fid = requestAnimationFrame(this.tick.call(this));
+    const now = Date.now();
+    this.d.getTime(now);
+    this.nodes.root = ReactDOM.findDOMNode(this);
+    this.nodes.min = this.nodes.root.querySelector('.v6don-estclock-permin');
+    this.prevTickMin = Math.floor(now / 60000);
+    if (!this.props.timeZone) {
+      this.nodes.sec = this.nodes.root.querySelector('.v6don-estclock-sec');
+      this.nodes.tick = this.nodes.root.querySelector('.v6don-estclock-tick');
+      this.prevTickSec = Math.floor(now / 1000);
+      this.prevTickHalfSec = Math.floor(now / 500);
+    }
+    this.fid = requestAnimationFrame(this.tick);
   }
 
   componentWillUnmount() {
     cancelAnimationFrame(this.fid);
   }
 
+  ftime(elem) {
+    const rtn = this.tzFmt[elem](this.d);
+    return rtn.length === 1 ? '0' + rtn : rtn;
+  }
+
+  fdate() {
+    return (this.props.timeZone ? '' : `${this.ftime('year')}-${this.ftime('month')}-`) + `${this.ftime('date')} ${this.ftime('hour')}:${this.ftime('minute')}`;
+  }
+
   tick() {
-    return () => {
-      const tickTest = Math.floor(Date.now() / 500);
-      if (tickTest > this.prevTick) {
-        this.setState({});
-        this.prevTick = tickTest;
+    const now = Date.now();
+    this.d.setTime(now);
+    if (this.nodes.sec) {
+      const tickSec = Math.floor(now / 1000);
+      const tickHalfSec = Math.floor(now / 500);
+      if (tickSec > this.prevTickSec) {
+        this.prevTickSec = tickSec;
+        this.nodes.root.setAttribute('datetime', this.d.toISOString().replace(/\.\d+/, ''));
+        this.nodes.sec.textContent = this.ftime('second');
       }
-      this.fid = requestAnimationFrame(this.tick.call(this));
-    };
+      if (tickHalfSec > this.prevTickHalfSec) {
+        this.prevTickHalfSec = tickHalfSec;
+        this.nodes.tick.setAttribute('style', tickHalfSec % 2 ? 'opacity: 0;' : '');
+      }
+    }
+    const tickMin = Math.floor(now / 60000);
+    if (tickMin > this.prevTickMin) {
+      this.prevTickMin = tickMin;
+      this.nodes.min.textContent = this.fdate();
+      if (!this.sec) {
+        this.nodes.root.setAttribute('datetime', this.d.toISOString().replace(/:\d+\.\d+/, ''));
+      }
+    }
+    this.fid = requestAnimationFrame(this.tick);
   }
 
   render() {
-    const cur = new Date();
-    return (<div className='v6don-estclock' data-tick={cur.getMilliseconds() >= 500 ? 'hidden' : 'visible'}><table><tbody>
-      <tr><th>Local:</th><td>{localtime(cur)}</td></tr>
-      <tr><th>Germany:</th><td>{est(cur)}</td></tr>
-    </tbody></table></div>);
+    const sec = this.props.timeZone ? null :
+      (<span>
+        <span className='v6don-estclock-tick'>:</span>
+        <span className='v6don-estclock-sec'>{this.ftime('second')}</span>
+      </span>);
+    return (<time dateTime={this.d.toISOString().replace(/\.\d+/, '')}>
+      <span className='v6don-estclock-permin'>{this.fdate()}</span>
+      {sec}
+    </time>);
   }
 
 }
+
+export default () => <p className='v6don-estclock'>Local: <Time />, <abbr title='Eugen Standard Time'>EST</abbr>: <Time timeZone='Europe/Berlin' /></p>;
